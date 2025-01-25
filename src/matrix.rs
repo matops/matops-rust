527872c390ddbcd36ef11b058ee357273df5d259
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2025-01-26 02:26:04 +0500

    Added matrix determinant method

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 use std::iter::Sum;
-use std::ops::{Add, Mul};
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 use crate::vector::Vector;
 
@@ -57,5 +57,57 @@ where
             data[i] = row_vector.dot(&rhs);
         }
         Vector { data }
+    }
+}
+
+impl<T, const M: usize> Matrix<T, M, M>
+where
+    T: Copy
+        + Default
+        + PartialEq
+        + Mul<Output = T>
+        + Add<Output = T>
+        + Sub<Output = T>
+        + Neg<Output = T>
+        + From<i32>
+        + Div<Output = T>,
+{
+    pub fn determinant(&self) -> T {
+        let mut mat = *self;
+        let mut det = T::from(1);
+        let mut sign = T::from(1);
+
+        for i in 0..M {
+            // Find pivot row
+            let mut pivot = i;
+            for j in i..M {
+                if mat.data[j][i] != T::default() {
+                    pivot = j;
+                    break;
+                }
+            }
+
+            if mat.data[pivot][i] == T::default() {
+                return T::default();
+            }
+
+            // Swap rows if needed
+            if pivot != i {
+                mat.data.swap(i, pivot);
+                sign = -sign;
+            }
+
+            // Eliminate lower rows
+            for j in (i + 1)..M {
+                let factor = mat.data[j][i] / mat.data[i][i];
+                for k in i..M {
+                    mat.data[j][k] = mat.data[j][k] - factor * mat.data[i][k];
+                }
+            }
+
+            det = det * mat.data[i][i];
+        }
+
+        det * sign
     }
 }
